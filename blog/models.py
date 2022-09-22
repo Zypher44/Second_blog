7b5769bc9c98a2e6183fd9c9ab1647f8f1be56b6
Radu <RaduIntuneric@gmail.com>
2022-09-22 18:03:34 -0400

    Added bootstraps and registration to blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from turtle import title
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here. What we save to database
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # Delete user will delete post
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
-posts = [
-    {
-        "author": "Radu",
-        "title": "BlogPost1",
-        "content": "First post for the mans",
-        "date_posted": "2022-09-21",
-    },
-    {
-        "author": "Radu",
-        "title": "BlogPost2",
-        "content": "Second post for the mans",
-        "date_posted": "2022-09-21",
-    },
-]
-
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
